f0e90f5a357c1f247962f8b6cd5b7a6b235c7a39
Ronan PEYREL <archib71@gmail.com>
2024-03-19 08:42:02 +0100

    contract v1

---
+++ contracts/todo.sol
@@ -0,0 +1,41 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.7.0 <0.9.0;
+// We have to specify what version of compiler this code will compile with
+contract todo {
+
+    struct Task {
+        uint id;
+        string content;
+        bool completed;
+        uint createdAt;
+    }
+
+    mapping(uint => Task) public tasks;
+    uint public taskCount;
+
+    function createTask(string memory _content) public {
+        taskCount ++;
+        tasks[taskCount] = Task(taskCount, _content, false, block.timestamp);
+        emit TaskCreated(taskCount, _content, false, block.timestamp);
+    }
+
+    function toggleCompleted(uint _id) public {
+        Task memory _task = tasks[_id];
+        _task.completed = !_task.completed;
+        tasks[_id] = _task;
+        emit TaskCompleted(_id, _task.completed);
+    }
+
+    event TaskCreated(
+        uint id,
+        string content,
+        bool completed,
+        uint createdAt
+    );
+
+    event TaskCompleted(
+        uint id,
+        bool completed
+    );
+
+}
